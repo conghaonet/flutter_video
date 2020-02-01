606d773df252d718be5d1a52f19b15e8065cfcfb
lex <>
2020-02-02 01:16:51 +0800

    upgrade to flutter v1.12.13+hotfix.5

--- android/app/src/main/kotlin/com/app2m/flutter_video/MainActivity.kt
+++ android/app/src/main/kotlin/com/app2m/flutter_video/MainActivity.kt
@@ -1,22 +1,12 @@
 package com.app2m.flutter_video
 
-import android.os.Bundle
-//import androidx.annotation.NonNull;
-//import io.flutter.embedding.android.FlutterActivity
-//import io.flutter.embedding.engine.FlutterEngine
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-import io.flutter.app.FlutterActivity
-
 class MainActivity: FlutterActivity() {
-/*
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
-*/
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
     }
 }
